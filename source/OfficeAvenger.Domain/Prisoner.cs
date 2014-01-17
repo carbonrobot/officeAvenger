a63dc73f10657b4e59dc0d816de6648e68ddd866
Charlie Brown <carbonrobot@gmail.com>
2014-01-17 14:58:17 -0600

    Added login page and supporting scaffolding

---
+++ source/OfficeAvenger.Domain/Prisoner.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace OfficeAvenger.Domain
+{
+    /// <summary>
+    /// Typical cubical bound office prisoner
+    /// </summary>
+    public class Prisoner
+    {
+        /// <summary>
+        /// Gets or sets the name of this prisoner
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("Name");
+
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the password hash
+        /// </summary>
+        public string PasswordHash
+        {
+            get { return _passwordHash; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("PasswordHash");
+
+                _passwordHash = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the username
+        /// </summary>
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException("Username");
+
+                _username = value;
+            }
+        }
+
+        private string _name;
+        private string _passwordHash;
+        private string _username;
+    }
+}
--- source/OfficeAvenger.Web/Areas/Razor/Controllers/HomeController.cs
+++ source/OfficeAvenger.Web/Areas/Razor/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace OfficeAvenger.Web.Areas.Razor.Controllers
 {
@@ -12,6 +13,14 @@ namespace OfficeAvenger.Web.Areas.Razor.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            //FormsAuthentication.SetAuthCookie(username, true);
+
+            return new RedirectResult("Index");
         }
 
     }
