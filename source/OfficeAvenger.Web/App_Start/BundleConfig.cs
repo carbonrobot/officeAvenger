fdda1a95857679478cbbe1ebb5c4ae947e925429
Charlie Brown <carbonrobot@gmail.com>
2014-01-17 11:50:47 -0600

    Setting up foundation css framework

--- source/OfficeAvenger.Web/App_Start/BundleConfig.cs
+++ source/OfficeAvenger.Web/App_Start/BundleConfig.cs
@@ -11,14 +11,20 @@ namespace OfficeAvenger.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/foundation/css").Include(
-                       "~/Content/foundation/foundation.css",
-                       "~/Content/foundation/foundation.mvc.css",
-                       "~/Content/foundation/app.css"));
+                       "~/Content/css/foundation/foundation.css",
+                       "~/Content/css/foundation/foundation.mvc.css",
+                       "~/Content/css/app.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
-                      "~/Scripts/foundation/foundation.js",
-                      "~/Scripts/foundation/foundation.*",
-                      "~/Scripts/foundation/app.js"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Scripts/vendor/modernizr.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "//code.jquery.com/jquery-2.0.3.min.js")
+                .Include("~/Scripts/vendor/jquery.js"));
+            
+            bundles.Add(new ScriptBundle("~/bundles/application").Include(
+                        "~/Scripts/vendor/foundation/foundation.js",
+                        "~/Scripts/vendor/foundation/foundation.topbar.js",
+                        "~/Scripts/app.js"));
         }
     }
 }
