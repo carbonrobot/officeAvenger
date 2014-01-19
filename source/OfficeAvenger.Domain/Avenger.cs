c753fa0fab07af67656bca519e4fe3fc70d69f0c
Charlie Brown <carbonrobot@gmail.com>
2014-01-19 00:23:35 -0600

    Edit avenger scaffolding

---
+++ source/OfficeAvenger.Domain/Avenger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OfficeAvenger.Domain
+{
+    public class Avenger
+    {
+        /// <summary>
+        /// Gets or sets the path to the avatar
+        /// </summary>
+        public string Avatar
+        {
+            get
+            {
+                return _avatar;
+            }
+            set
+            {
+                _avatar = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentOutOfRangeException();
+
+                _name = value;
+            }
+        }
+
+        private string _avatar;
+        private string _name;
+    }
+}
---
+++ source/OfficeAvenger.Web/Areas/Razor/Controllers/TeamController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OfficeAvenger.Web.Areas.Razor.Models;
+
+namespace OfficeAvenger.Web.Areas.Razor.Controllers
+{
+    public class TeamController : Controller
+    {
+        public ActionResult Add()
+        {
+            return View("Edit");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EditTeamModel model)
+        {
+            return View();
+        }
+
+    }
+}
