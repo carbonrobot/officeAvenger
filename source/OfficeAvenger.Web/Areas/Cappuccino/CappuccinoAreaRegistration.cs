352453e4a9c6dfbabb227a4f1c82a87e5d3e92ad
ryibas <ryan.baseman@gmail.com>
2014-02-04 08:46:20 -0600

    Starting cappuccino shell
    Adding the start of cappuccino.

---
+++ source/OfficeAvenger.Web/Areas/Cappuccino/CappuccinoAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace OfficeAvenger.Web.Areas.Cappuccino
+{
+    public class CappuccinoAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Cappuccino";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Cappuccino_default",
+                "Cappuccino/{controller}/{action}",
+                new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
---
+++ source/OfficeAvenger.Web/Areas/Cappuccino/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using OfficeAvenger.Web.Areas.Razor.Models;
+using OfficeAvenger.Web.Controllers;
+using OfficeAvenger.Web.Security;
+
+namespace OfficeAvenger.Web.Areas.Cappuccino.Controllers
+{
+    public class HomeController : UnderworldController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var authenticated = OfficeAvenger.Web.Security.Handlers.FormsAuthentication.Authenticate(username, password, true);
+            if (!authenticated)
+            {
+                return WithError(this.RedirectToAction("Index"), "Incorrect. Do not fail again.");
+            }
+            return this.RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            OfficeAvenger.Web.Security.Handlers.FormsAuthentication.Signout();
+            return RedirectToAction("Index");
+        }
+    }
+}
