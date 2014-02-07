51d9d176ddc0f2f0f18db983038958bce2950521
Charlie Brown <carbonrobot@gmail.com>
2014-02-07 15:42:53 -0600

    Added Angular dummy data and bindings

--- source/OfficeAvenger.Web/Scripts/Areas/Angular/app.js
+++ source/OfficeAvenger.Web/Scripts/Areas/Angular/app.js
@@ -1,1 +1,24 @@
 ﻿
+var officeAvengerApp = angular.module('officeAvengerApp', []);
+officeAvengerApp.controller('TeamController', function ($scope) {
+
+    $scope.team = [
+        {
+            Name: 'Iron Man',
+            Avatar: ''
+        },
+        {
+            Name: 'Thor',
+            Avatar: ''
+        },
+        {
+            Name: 'Captain America',
+            Avatar: ''
+        }
+    ];
+
+});
+
+$(function () {
+
+});
